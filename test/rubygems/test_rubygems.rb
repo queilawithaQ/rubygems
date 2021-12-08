0e9336ed663f3a8fa542ea7f381d2af9dd884390
David Rodríguez <deivid.rodriguez@riseup.net>
2021-12-08 21:45:16 +0100

    Load `operating_system.rb` customizations before setting up default gems
    It's very common for packagers to configure gem paths in this file, for
    example, `Gem.default_dir`. Also, setting up default gems requires these
    paths to be set, so that we know which default gems need to be setup.
    
    If we setup default gems before loading `operatin_system.rb`
    customizations, the wrong default gems will be setup.
    
    Unfortunately, default gems loaded by `operating_system.rb` can't be
    upgraded if we do this, but it seems much of a smaller issue. I wasn't
    even fully sure it was the right thing to do when I added that, and it
    was not the culprit of the end user issue that led to making that
    change.

--- test/rubygems/test_rubygems.rb
+++ test/rubygems/test_rubygems.rb
@@ -22,6 +22,23 @@ class GemTest < Gem::TestCase
     "the problem and ask for help."
   end
 
+  def test_operating_system_customizing_default_dir
+    pend "does not apply to truffleruby" if RUBY_ENGINE == 'truffleruby'
+
+    # On a non existing default dir, there should be no gems
+
+    path = util_install_operating_system_rb <<-RUBY
+      module Gem
+        def self.default_dir
+          @homebrew_path ||= File.expand_path("foo")
+        end
+      end
+    RUBY
+
+    output = Gem::Util.popen(*ruby_with_rubygems_and_fake_operating_system_in_load_path(path), '-S', "gem", "list", {:err => [:child, :out]}).strip
+    assert_empty output
+  end
+
   private
 
   def util_install_operating_system_rb(content)
